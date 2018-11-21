d22f3af9b0ee3130d38b7f21be1afa65434b5d1b
Junajpu M <junaj007@hotmail.com>
2018-11-21 10:26:32 -0600

    Video 2.5 Recursos estaticos.

--- grocerystore/src/main/java/edu/umg/grocerystore/controller/PageController.java
+++ grocerystore/src/main/java/edu/umg/grocerystore/controller/PageController.java
@@ -1,18 +1,21 @@
 package edu.umg.grocerystore.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController {
 	
-	@RequestMapping(value= {"/","home","index"})
+	@RequestMapping(value= {"/","/home","/index"})
+	
 	public ModelAndView index() {
+	
+		
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "Bienvenido a Spring Web MVC");
+		
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
 		return mv;
 	}
 /*	@RequestMapping(value= {"/test"})
